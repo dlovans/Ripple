255013c2bea57515475b0bbaeace515fb9b1c851
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-19 21:31:59 +0100

    Initial Commit

---
+++ Ripple/RippleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RippleApp.swift
+//  Ripple
+//
+//  Created by Dlovan Sharif on 2024-11-19.
+//
+
+import SwiftUI
+
+@main
+struct RippleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
