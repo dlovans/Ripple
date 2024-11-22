cc81cf647aceabea26359c28291578190234072e
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-11-22 16:34:44 +0100

    Create menu view

---
+++ Ripple/Views/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  Ripple
+//
+//  Created by Dlovan Sharif on 2024-11-22.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
