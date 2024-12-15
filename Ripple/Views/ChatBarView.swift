feeb7162aeacf3487574c3d727694f862cdd14f6
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-15 20:53:05 +0100

    Update .gitignore to exclude Firebase config file

---
+++ Ripple/Views/ChatBarView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatBarView.swift
+//  Ripple
+//
+//  Created by Dlovan Sharif on 2024-11-27.
+//
+
+import SwiftUI
+
+struct ChatBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatBarView()
+}
