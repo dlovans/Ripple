1ea7e389126b672f163bd8970b706b6c0da0567c
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-17 19:23:45 +0100

    Update Auth view

--- Ripple/Views/AuthView/AuthView.swift
+++ Ripple/Views/AuthView/AuthView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct AuthView: View {
+    let authType: AuthType
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack (alignment: .top) {
+            Color.stone
+                .ignoresSafeArea()
+            VStack (alignment: .leading) {
+                VStack (alignment: .leading) {
+                    Text(authType == .login ? "Welcome Back!" : "Get started with Ripple!")
+                        .font(.title)
+                        .foregroundStyle(.white)
+                    Text(authType == .login ? "Login to start chatting." : "Join and start chatting.")
+                        .font(.title2)
+                        .foregroundStyle(.white)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                Spacer()
+                AuthFieldsView(authType: authType)
+                
+                VStack {
+                    AuthProviderView(authProvider: .Apple, authType: .login, iconPath: "Apple")
+                    AuthProviderView(authProvider: .Google, authType: .login, iconPath: "Google")
+                }
+                .padding(.top, 40)
+                
+                HStack {
+                    Text("\(authType == .login ? "Don't have an account?" : "Already got an account?")")
+                        .foregroundStyle(Color.gray)
+                    Button {
+                        print("switching")
+                    } label: {
+                        Text("\(authType == .login ? "Join" : "Login")")
+                            .foregroundStyle(.emerald)
+                    }
+                }
+                .padding(.top, 15)
+                .frame(maxWidth: .infinity)
+                
+                Spacer()
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+        }
     }
 }
 
 #Preview {
-    AuthView()
+    AuthView(authType: .register)
 }
