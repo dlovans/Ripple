6c88914f3a8c22dec02c698974e7b4b0b8cdb3a7
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-17 19:25:48 +0100

    Embed ChatView code in VStack

--- Ripple/Views/ChatView/ChatView.swift
+++ Ripple/Views/ChatView/ChatView.swift
@@ -9,22 +9,26 @@ import SwiftUI
 
 struct ChatView: View {    
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: false)
-                ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: false)
-                ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
-                ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
-                ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
-                ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
-                ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
-                ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
-                ChatMessageView(username: "Pablito", message: "Whasfffgoin on", isMe: true)
+        VStack {
+            Text("Edingekroken")
+                .foregroundStyle(.white)
+            ScrollView {
+                LazyVStack {
+                    ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: false)
+                    ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: false)
+                    ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
+                    ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
+                    ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
+                    ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
+                    ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
+                    ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
+                    ChatMessageView(username: "Pablito", message: "Whasfffgoin on", isMe: true)
+                }
             }
+            .defaultScrollAnchor(.bottom)
+            .scrollIndicators(.hidden)
+            ChatFieldView()
         }
-        .defaultScrollAnchor(.bottom)
-        .scrollIndicators(.hidden)
-        ChatBarView()
     }
 }
 
