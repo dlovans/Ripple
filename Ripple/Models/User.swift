f67040a4029d5c633ac78cf0b97bdb3a30af80e9
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-18 20:28:15 +0100

    Declare User model

---
+++ Ripple/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Ripple
+//
+//  Created by Dlovan Sharif on 2024-12-18.
+//
+
+import Foundation
+
+struct User {
+    var id: String
+    var username: String
+    var isPremium: Bool = false
+}
