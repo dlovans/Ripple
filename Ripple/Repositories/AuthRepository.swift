98f417a51a4658e4fbdb7b13e761bd2ed6073c67
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-19 23:41:44 +0100

    Define method to signup user with email and password

--- Ripple/Repositories/AuthRepository.swift
+++ Ripple/Repositories/AuthRepository.swift
@@ -25,5 +25,31 @@ class AuthRepository {
             completion(.success(authResult))
         }
     }
+    
+    /**
+     Signs up user with email and password.
+     - Parameters:
+        - email: The email to sign up with.
+        - password: User-inputted password.
+        - completion: A closure called when the sign up process completes. It takes a Result type:
+            - `.success(AuthDataResult)`: Contains the authentication result when the sign up is successful.
+            - `.failure(Error)`: Contains the error that occurred if the sign up fails.
+     */
+    func signupWithEmailAndPassword(email: String, password: String, completion: @escaping (Result<AuthDataResult, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let authResult = authResult else {
+                let authError = NSError(domain: "AuthError", code: -1, userInfo: nil)
+                completion(.failure(authError))
+                return
+            }
+            
+            completion(.success(authResult))
+        }
+    }
 }
 
