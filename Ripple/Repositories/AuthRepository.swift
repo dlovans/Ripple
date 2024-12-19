7e3bf74ac35b5d9612670db81e682bbe57f1ad56
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-19 12:15:31 +0100

    Define method for signing in user with email and password

--- Ripple/Repositories/AuthRepository.swift
+++ Ripple/Repositories/AuthRepository.swift
@@ -6,7 +6,24 @@
 //
 
 import Foundation
+import FirebaseAuth
 
 class AuthRepository {
-    
+    func loginWithEmailAndPassword(email: String, password: String, completion: @escaping (Result<AuthDataResult, Error>) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let authResult = authResult else {
+                let authError = NSError(domain: "AuthError", code: -1, userInfo: nil)
+                completion(.failure(authError))
+                return
+            }
+            
+            completion(.success(authResult))
+        }
+    }
 }
+
