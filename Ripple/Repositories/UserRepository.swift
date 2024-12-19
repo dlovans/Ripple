8a7399eecd9f5099f3ce3958f7cf660b95c73c61
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-19 23:42:05 +0100

    Declare UserRepository

---
+++ Ripple/Repositories/UserRepository.swift
@@ -0,0 +1,42 @@
+//
+//  UserRepository.swift
+//  Ripple
+//
+//  Created by Dlovan Sharif on 2024-12-19.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class UserRepository {
+    let db = Firestore.firestore()
+    
+    func createUser(userId: String) async -> User? {
+        let docRef = db.collection("users").document(userId)
+        
+        do {
+            let document = try await docRef.getDocument()
+            
+            if document.exists, let data = document.data() {
+                if let username = data["username"] as? String,
+                   let isPremium = data["isPremium"] as? Bool {
+                    return User(id: userId, username: username, isPremium: isPremium)
+                } else {
+                    print("Error: Missing or invalid fields in document.")
+                    return nil
+                }
+            } else {
+                try await db.collection("users").document(userId).setData([
+                    "username": "",
+                    "isPremium": false
+                ])
+                
+                return User(id: userId, username: "", isPremium: false)
+            }
+        } catch {
+            print("Error fetching user data: \(error)")
+            return nil
+        }
+    }
+
+}
