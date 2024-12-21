3e33c642c6306c794a7886ba3fde1f14829ee20a
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-21 13:20:28 +0100

    Declare ChatRepository

---
+++ Ripple/Repositories/ChatRepository.swift
@@ -0,0 +1,33 @@
+//
+//  ChatRepository.swift
+//  Ripple
+//
+//  Created by Dlovan Sharif on 2024-12-20.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class ChatRepository {
+    let db = Firestore.firestore()
+    
+    func createMessage(message: String, username: String, userId: String, isPremium: Bool, chatId: String) async -> Bool {
+        let chatRef = db.collection("chats").document(chatId).collection("messages")
+        
+        let messageData: [String: Any] = [
+            "message": message,
+            "username": username,
+            "userId": userId,
+            "isPremium": isPremium,
+            "timestamp": FieldValue.serverTimestamp()
+        ]
+            
+        
+        do {
+            try await chatRef.addDocument(data: messageData)
+            return true
+        } catch {
+            return false
+        }
+    }
+}
