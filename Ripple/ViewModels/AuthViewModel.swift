d2b70b8b28a464a0ae588b84a5498549c190e749
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-23 21:40:32 +0100

    Add a isLoading to AuthViewModel to prevent view swapping

--- Ripple/ViewModels/AuthViewModel.swift
+++ Ripple/ViewModels/AuthViewModel.swift
@@ -10,13 +10,17 @@ import FirebaseAuth
 
 class AuthViewModel: ObservableObject {
     @Published var isAuthenticated: Bool = false
+    @Published var isLoading: Bool = true
     
     let authRepository = AuthRepository()
     var handle: AuthStateDidChangeListenerHandle?
 
     init() {
         handle = Auth.auth().addStateDidChangeListener{ [weak self] _, user in
-            self?.isAuthenticated = user != nil
+            DispatchQueue.main.async {
+                self?.isAuthenticated = user != nil
+                self?.isLoading = false
+            }
         }
     }
     
