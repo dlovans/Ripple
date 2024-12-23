35e819669b441c5cec2be2f892f278d1a2cf791d
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-23 21:41:47 +0100

    Declare SpinnerView to indicate loading

---
+++ Ripple/Views/SpinnerView.swift
@@ -0,0 +1,25 @@
+//
+//  ProgressView.swift
+//  Ripple
+//
+//  Created by Dlovan Sharif on 2024-12-21.
+//
+
+import SwiftUI
+
+struct SpinnerView: View {
+    var body: some View {
+        ZStack {
+            Color.stone
+                .ignoresSafeArea(edges: .all)
+            ProgressView()
+              .progressViewStyle(CircularProgressViewStyle(tint: .emerald))
+              .scaleEffect(2.0, anchor: .center)
+        }
+
+    }
+}
+
+#Preview {
+    SpinnerView()
+}
