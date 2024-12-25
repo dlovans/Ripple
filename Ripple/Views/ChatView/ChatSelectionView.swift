6ccb44646f45ddd19ef2b5c5ed96f9382be27c17
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-25 23:33:16 +0100

    Update ChatSelectionView navigation and UI

--- Ripple/Views/ChatView/ChatSelectionView.swift
+++ Ripple/Views/ChatView/ChatSelectionView.swift
@@ -11,46 +11,52 @@ struct ChatSelectionView: View {
     @EnvironmentObject var locationViewModel: LocationViewModel
     @State private var displayCreateChat: Bool = false
     @State private var createChatTitle: String = ""
+    @State private var navigateToChat: Bool = false
     
     var body: some View {
-        ZStack {
-            Color.stone
-                .ignoresSafeArea(.all)
-            VStack {
-                HStack {
-                    Button {
-                        displayCreateChat.toggle()
-                    } label: {
-                        HStack {
-                            Image(systemName: "plus")
-                            Text("New Chat")
+        NavigationStack {
+            ZStack {
+                Color.stone
+                    .ignoresSafeArea(.all)
+                VStack {
+                    HStack {
+                        Button {
+                            displayCreateChat.toggle()
+                        } label: {
+                            HStack {
+                                Image(systemName: "plus")
+                                Text("New Chat")
+                            }
+                        }
+                        .sheet(isPresented: $displayCreateChat) {
+                            ChatCreateView(navigateToChat: $navigateToChat, isPresented: $displayCreateChat)
                         }
                     }
-                    .sheet(isPresented: $displayCreateChat) {
-                        ChatCreateView(isPresented: $displayCreateChat)
-                    }
-                }
-                if locationViewModel.isLoading {
-                    SpinnerView()
-                } else {
-                    ScrollView {
-                        LazyVStack {
-                            ForEach(locationViewModel.localizedChats) {chatItem in
-                                Text(chatItem.chatName)
-                                    .foregroundStyle(.textcolor)
+                    if locationViewModel.isLoading {
+                        SpinnerView()
+                    } else {
+                        ScrollView {
+                            LazyVStack {
+                                ForEach(locationViewModel.localizedChats) {chatItem in
+                                    Text(chatItem.chatName)
+                                        .foregroundStyle(.textcolor)
+                                }
                             }
                         }
                     }
                 }
+                .frame(maxHeight: .infinity)
+                .padding()
+                .navigationDestination(isPresented: $navigateToChat) {
+                    ChatView(navigateToChat: $navigateToChat)
+                }
             }
-            .frame(maxHeight: .infinity)
-            .padding()
-        }
-        .onAppear {
-            locationViewModel.startPeriodicLocationTask(locationMode: .chats, intervalInSeconds: 60)
-        }
-        .onDisappear {
-            locationViewModel.stopPeriodicLocationTask()
+            .onAppear {
+                locationViewModel.startPeriodicLocationTask(locationMode: .chats)
+            }
+            .onDisappear {
+                locationViewModel.stopPeriodicLocationTask()
+            }
         }
     }
 }
