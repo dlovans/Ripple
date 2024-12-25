f5a65870752ae3b3c277d0b61819141c171c26ec
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-25 17:30:03 +0100

    Refactor LocationViewModel and add method fetching chats and events based on timer

--- Ripple/ViewModels/LocationViewModel.swift
+++ Ripple/ViewModels/LocationViewModel.swift
@@ -11,7 +11,13 @@ import CoreLocation
 class LocationViewModel: NSObject, CLLocationManagerDelegate, ObservableObject {
     @Published var locationAuthorized: CLAuthorizationStatus = .notDetermined
     @Published var lastKnownLocation: CLLocationCoordinate2D?
-    var manager = CLLocationManager()
+    @Published var localizedChats: [Chat] = []
+    @Published var localizedEvents: [Event] = []
+    @Published var isLoading: Bool = true
+    
+    private var locationRepository: LocationRepository = LocationRepository()
+    private var queryTimer: Timer?
+    private var manager = CLLocationManager()
     
     override init() {
         super.init()
@@ -27,25 +33,63 @@ class LocationViewModel: NSObject, CLLocationManagerDelegate, ObservableObject {
             locationAuthorized = manager.authorizationStatus
         case .authorizedWhenInUse, .authorizedAlways:
             locationAuthorized = manager.authorizationStatus
-            startUpdatingLocation()
         @unknown default:
             locationAuthorized = .notDetermined
         }
     }
     
-    func startUpdatingLocation() {
-        manager.startUpdatingLocation()
+    func getDataByLocation(locationMode: LocationMode) async {
+        switch locationMode {
+        case .events:
+            if let location = self.lastKnownLocation {
+                let events = await self.locationRepository.getEvents(location: location)
+                await MainActor.run {
+                    self.localizedEvents = events
+                }
+            }
+        case .chats:
+            if let location = self.lastKnownLocation {
+                let chats =  await self.locationRepository.getChats(location: location)
+                await MainActor.run {
+                    self.localizedChats = chats
+                }
+            }
+        }
     }
     
-    func stopUpdatingLocation() {
-        manager.stopUpdatingLocation()
+    func startPeriodicLocationTask(locationMode: LocationMode, intervalInSeconds: Double) {
+        queryTimer?.invalidate()
+        manager.startUpdatingLocation()
+        queryTimer = Timer.scheduledTimer(withTimeInterval: intervalInSeconds, repeats: true) { [weak self] _ in
+            self?.manager.requestLocation()
+            Task { @MainActor in
+                await self?.getDataByLocation(locationMode: locationMode)
+                self?.isLoading = false
+            }
+        }
+        queryTimer?.fire()
+    }
+
+    
+    func stopPeriodicLocationTask () {
+        queryTimer?.invalidate()
+        queryTimer = nil
+        self.isLoading = true
     }
         
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         checkLocationAuthorization()
     }
     
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get location: \(error.localizedDescription)")
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         lastKnownLocation = locations.first?.coordinate
+    }
+    
+    deinit {
+        self.stopPeriodicLocationTask()
     }
 }
