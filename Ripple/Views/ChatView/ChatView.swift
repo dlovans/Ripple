4736b715c48a2b3efecd4e725d3ce5639dd3647e
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-25 17:31:13 +0100

    Update ChatView UI

--- Ripple/Views/ChatView/ChatView.swift
+++ Ripple/Views/ChatView/ChatView.swift
@@ -13,17 +13,45 @@ struct ChatView: View {
     @EnvironmentObject var userViewModel: UserViewModel
     @EnvironmentObject var chatViewModel: ChatViewModel
     
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         ZStack {
             Color.stone
                 .ignoresSafeArea()
             VStack {
-                Text("Edingekroken")
-                    .foregroundStyle(.textcolor)
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "arrow.left")
+                            .foregroundStyle(.emerald)
+                    }
+                    Spacer()
+
+                    Text("Connections: \(chatViewModel.chat?.connections ?? 1)")
+                        .foregroundStyle(.textcolor)
+                }
+                .padding()
                 ScrollView {
                     LazyVStack {
                         // ForEach. isMe = userViewModel?.user.id == message.userId
                         
+                        ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: false)
+                        ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: false)
+                        ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
+                        ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
+                        ChatMessageView(username: "Pablito", message: "Whasfffgoin on", isMe: true)
+                        ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: false)
+                        ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: false)
+                        ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
+                        ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
+                        ChatMessageView(username: "Pablito", message: "Whasfffgoin on", isMe: true)
+                        ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: false)
+                        ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: false)
+                        ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
+                        ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
+                        ChatMessageView(username: "Pablito", message: "Whasfffgoin on", isMe: true)
                         ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: false)
                         ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: false)
                         ChatMessageView(username: "Pablito", message: "Whas goin on", isMe: true)
