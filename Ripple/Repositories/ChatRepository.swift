9621ed37e8617536e2a4b3bdc45878eaed39dc92
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-25 23:27:09 +0100

    Define methods for creating a geo chat

--- Ripple/Repositories/ChatRepository.swift
+++ Ripple/Repositories/ChatRepository.swift
@@ -7,27 +7,82 @@
 
 import Foundation
 import FirebaseFirestore
+import CoreLocation
 
 class ChatRepository {
     let db = Firestore.firestore()
-    
-    func createMessage(message: String, username: String, userId: String, isPremium: Bool, chatId: String) async -> Bool {
-        let chatRef = db.collection("chats").document(chatId).collection("messages")
         
-        let messageData: [String: Any] = [
-            "message": message,
-            "username": username,
-            "userId": userId,
-            "isPremium": isPremium,
-            "timestamp": FieldValue.serverTimestamp()
+    func createChat(chatName: String, zoneSize: ZoneSize, location: CLLocationCoordinate2D, maxConnections: Int) async -> Chat? {
+        let geoData = calculateBoundingBox(center: location, zoneSize: zoneSize)
+        let chatData: [String: Any] = [
+            "name": chatName,
+            "connections": 1,
+            "maxConnection": maxConnections,
+            "timestamp": FieldValue.serverTimestamp(),
+            "longStart": geoData.longStart,
+            "longEnd": geoData.longEnd,
+            "latStart": geoData.latStart,
+            "latEnd": geoData.latEnd
         ]
-            
         
         do {
-            try await chatRef.addDocument(data: messageData)
-            return true
+            let chatRef = try await db.collection("chats").addDocument(data: chatData)
+            
+            struct Message: Identifiable, Codable {
+                let id: String?
+                let userId: String?
+                let username: String
+                let message: String
+                let isMe: Bool
+                let isPremium: Bool
+            }
+
+            try await chatRef.collection("messages").addDocument(data: [
+                "userId": "system",
+                "username": "Admin",
+                "timestamp": FieldValue.serverTimestamp(),
+                "message": "Welcome...Keep it civil.",
+                "isPremium": true
+            ])
+            
+            return Chat(
+                id: chatRef.documentID,
+                chatName: chatName,
+                connections: 1,
+                maxConnections: maxConnections,
+                longStart: geoData.longStart,
+                longEnd: geoData.longEnd,
+                latStart: geoData.latStart,
+                latEnd: geoData.latEnd
+            )
         } catch {
-            return false
+            print("Failed creating chat.")
+            return nil
         }
     }
+    
+    func calculateBoundingBox(center: CLLocationCoordinate2D, zoneSize: ZoneSize) -> (latStart: Double, latEnd: Double, longStart: Double, longEnd: Double) {
+        let distance: Double
+        switch zoneSize {
+        case .small:
+            distance = 3.0
+        case .medium:
+            distance = 5.0
+        case .large:
+            distance = 10.0
+        case .extraLarge:
+            distance = 15.0
+        }
+
+        let deltaLat = distance / 111.0
+        let deltaLong = distance / (111.0 * cos(center.latitude * .pi / 180.0))
+
+        let latStart = center.latitude - deltaLat
+        let latEnd = center.latitude + deltaLat
+        let longStart = center.longitude - deltaLong
+        let longEnd = center.longitude + deltaLong
+
+        return (latStart, latEnd, longStart, longEnd)
+    }
+
 }
