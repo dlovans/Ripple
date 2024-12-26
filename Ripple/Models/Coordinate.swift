ef3d8cbde75c9d227887a48a40b65e54cffe5945
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-26 12:33:50 +0100

    Declare a coordination model

---
+++ Ripple/Models/Coordinate.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinate.swift
+//  Ripple
+//
+//  Created by Dlovan Sharif on 2024-12-26.
+//
+
+import Foundation
+
+struct Coordinate: Equatable {
+    var latitude: Double
+    var longitude: Double
+}
