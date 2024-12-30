b3b02f1344b2c650904bb00bac35beccbe3c7c9f
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-30 13:57:25 +0100

    Define method attaching listener to a chat's messages in MessageViewModel

--- Ripple/ViewModels/MessageViewModel.swift
+++ Ripple/ViewModels/MessageViewModel.swift
@@ -6,15 +6,34 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 class MessageViewModel: ObservableObject {
+    @Published var messages: [Message] = []
     private let messageRepository: MessageRepository = MessageRepository()
+    
+    private var messagesListener: ListenerRegistration?
+    
+    func subscribeToMessages(chatId: String) {
+        messagesListener = messageRepository.getMessages(chatId: chatId) { [weak self] messages in
+            Task { @MainActor in
+                self?.messages = messages
+            }
+        }
+    }
+    
+    func unsubscribeFromMessages() {
+        messagesListener?.remove()
+        messagesListener = nil
+        self.messages.removeAll()
+    }
     
     func addMessage(message: String, username: String, userId: String, isPremium: Bool, chatId: String) async {
         let messageStatus = await messageRepository.createMessage(message: message, username: username, userId: userId, isPremium: isPremium, chatId: chatId)
         
         if messageStatus {
             print("Successfully created a new message in DB.")
+            
         } else {
             print("Failed to create a new message in DB.")
         }
