975adc8524266f07bad1ca019f353a3c7eb75c2a
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-30 13:55:40 +0100

    Refactor ChatViewModel and define methods for updating chat connections

--- Ripple/ViewModels/ChatViewModel.swift
+++ Ripple/ViewModels/ChatViewModel.swift
@@ -14,38 +14,70 @@ class ChatViewModel: ObservableObject {
     @Published var chat: Chat?
     @Published var chats: [Chat]?
     @Published var localizedChats: [Chat] = []
-    @Published var isLoading: Bool = false
+    @Published var isLoading: Bool = true
+    @Published var chatIsLoading: Bool = true
     
     private let chatRepository: ChatRepository = ChatRepository()
+    private var chatsListener: ListenerRegistration?
     private var chatListener: ListenerRegistration?
+    
+    func startListeningToChat(for chatId: String) {
+        chatListener = chatRepository.fetchChat(chatId: chatId) { [weak self] chat in
+            Task { @MainActor in
+                self?.chat = chat
+                self?.chatIsLoading = false
+            }
+        }
+    }
+    
+    func stopListeningToChat() {
+        chatListener?.remove()
+        chatListener = nil
+        chatIsLoading = true
+    }
+    
+    func incrementConnection(for chatId: String) async {
+        do {
+            try await chatRepository.incrementConnection(chatId: chatId)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func decrementConnection(for chatId: String) async {
+        do {
+            try await chatRepository.decrementConnection(chatId: chatId)
+        } catch {
+            print(error)
+        }
+    }
     
     func startFetchingChats(center: Coordinate?) {
         guard let center else {
             print("No location provided.")
             return
         }
-            stopFetchingChats()
-
-        chatListener = chatRepository.fetchChats(center: center) { [weak self] chats in
-                Task { @MainActor in
-                    self?.localizedChats = chats
-                }
+        
+        chatsListener = chatRepository.fetchChats(center: center) { [weak self] chats in
+            Task { @MainActor in
+                self?.localizedChats = chats
             }
         }
-
-        func stopFetchingChats() {
-            chatListener?.remove()
-            chatListener = nil
-        }
-
+        isLoading = false
+    }
+    
+    func stopFetchingChats() {
+        chatsListener?.remove()
+        chatsListener = nil
+        isLoading = true
+    }
+    
     
     func createChat(chatName: String, zoneSize: ZoneSize, location: Coordinate, maxConnections: Int) async -> Bool {
-        let createdChat = await chatRepository.createChat(chatName: chatName, zoneSize: zoneSize, location: location, maxConnections: maxConnections)
+        let createdChatId = await chatRepository.createChat(chatName: chatName, zoneSize: zoneSize, location: location, maxConnections: maxConnections)
         
-        if let createdChat = createdChat {
-            await MainActor.run {
-                self.chat = createdChat
-            }
+        if let createdChatId {
+            self.startListeningToChat(for: createdChatId)
             return true
         } else {
             return false
