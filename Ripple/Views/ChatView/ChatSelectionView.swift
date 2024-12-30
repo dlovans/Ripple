8a7119176cef6619007225fdf842d5c91335d7b7
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-30 14:00:23 +0100

    Refactor and fix ChatSelectionView

--- Ripple/Views/ChatView/ChatSelectionView.swift
+++ Ripple/Views/ChatView/ChatSelectionView.swift
@@ -17,62 +17,76 @@ struct ChatSelectionView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                Color.stone
-                    .ignoresSafeArea(.all)
-                VStack {
-                    HStack {
-                        Button {
-                            displayCreateChat.toggle()
-                        } label: {
-                            HStack {
-                                Image(systemName: "plus")
-                                Text("New Chat")
+                if chatViewModel.chatIsLoading && locationService.isLoading {
+                    SpinnerView()
+                } else {
+                    Color.stone
+                        .ignoresSafeArea(.all)
+                    VStack {
+                        HStack {
+                            Button {
+                                displayCreateChat.toggle()
+                            } label: {
+                                HStack {
+                                    Image(systemName: "plus")
+                                    Text("New Chat")
+                                }
+                            }
+                            .sheet(isPresented: $displayCreateChat) {
+                                ChatCreateView(navigateToChat: $navigateToChat, isPresented: $displayCreateChat)
                             }
                         }
-                        .sheet(isPresented: $displayCreateChat) {
-                            ChatCreateView(navigateToChat: $navigateToChat, isPresented: $displayCreateChat)
-                        }
-                    }
-                    if chatViewModel.isLoading {
-                        SpinnerView()
-                    } else {
                         ScrollView {
                             LazyVStack {
                                 ForEach(chatViewModel.localizedChats) {chatItem in
-                                    Text(chatItem.chatName)
-                                        .foregroundStyle(.textcolor)
+                                    if chatItem.connections < chatItem.maxConnections {
+                                        ChatItemView(
+                                            navigateToChat: $navigateToChat,
+                                            title: chatItem.chatName,
+                                            connections: chatItem.connections,
+                                            maxConnections: chatItem.maxConnections,
+                                            chatId: chatItem.id
+                                        )
+                                    }
                                 }
                             }
                         }
                     }
-                }
-                .frame(maxHeight: .infinity)
-                .padding()
-                .navigationDestination(isPresented: $navigateToChat) {
-                    ChatView(navigateToChat: $navigateToChat)
-                        .navigationBarBackButtonHidden(true)
-                }
-            }
-            .onAppear {
-                locationService.startPeriodicLocationTask()
-                Task { @MainActor in
-                    chatViewModel.startFetchingChats(center: locationService.lastKnownLocation ?? nil)
-                }
-                print("created")
-            }
-            .onDisappear {
-                locationService.stopPeriodicLocationTask()
-                chatViewModel.stopFetchingChats()
-                print("destroyed1111")
-            }
-            .onChange(of: locationService.lastKnownLocation) { oldValue, newValue in
-                if oldValue != newValue {
-                    print("location changed")
-                    Task { @MainActor in
-                        chatViewModel.startFetchingChats(center: locationService.lastKnownLocation ?? nil)
+                    .frame(maxHeight: .infinity)
+                    .padding()
+                    .navigationDestination(isPresented: $navigateToChat) {
+                        ChatView(navigateToChat: $navigateToChat)
+                            .navigationBarBackButtonHidden(true)
+                    }
+                    .onAppear {
+                        Task { @MainActor in
+                            chatViewModel.startFetchingChats(center: locationService.lastKnownLocation ?? nil)
+                        }
+                    }
+                    .onDisappear {
+                        print("ChatSelectionView destroyed.")
+                        locationService.stopPeriodicLocationTask()
+                        chatViewModel.stopFetchingChats()
+                    }
+                    .onChange(of: locationService.lastKnownLocation) { oldValue, newValue in
+                        if let oldValue, let newValue {
+                            let tolerance = 0.000001
+
+                            if abs(oldValue.latitude - newValue.latitude) > tolerance || abs(oldValue.longitude - newValue.longitude) > tolerance {
+                                print("Updating location: latitude: \(locationService.lastKnownLocation?.latitude ?? 0), longitude: \(locationService.lastKnownLocation?.longitude ?? 0)")
+                                Task { @MainActor in
+                                    chatViewModel.stopFetchingChats()
+                                    chatViewModel.startFetchingChats(center: locationService.lastKnownLocation ?? nil)
+                                }
+                            }
+
+                        }
                     }
                 }
             }
+        }
+        .onAppear {
+            locationService.startPeriodicLocationTask()
         }
     }
 }
