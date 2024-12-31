c0ec4b0f3fd4a69de4b87409055fda929e1d45b1
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-12-31 13:59:04 +0100

    Remove unreliable method

--- Ripple/RippleApp.swift
+++ Ripple/RippleApp.swift
@@ -19,11 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         return true
     }
-    
-//    func applicationWillTerminate(_ application: UIApplication) {
-//        chatViewModel.decrementConnection(for: chatViewModel.chat.id)
-//        chatViewModel.stopListeningToChat()
-//    }
 }
 
 @main
