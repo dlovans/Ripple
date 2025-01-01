22055a6fb2e077e6e3934de89ea6e9d9acb27cef
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-01-01 20:56:57 +0100

    Declare empty ChatItemReportView for reporting a chat item

---
+++ Ripple/Views/ChatView/ChatItemReportView.swift
@@ -0,0 +1,22 @@
+//
+//  ChatReportView.swift
+//  Ripple
+//
+//  Created by Dlovan Sharif on 2025-01-01.
+//
+
+import SwiftUI
+
+struct ChatItemReportView: View {
+    let chatId: String
+    
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ChatItemReportView(chatId: "chatId")
+}
