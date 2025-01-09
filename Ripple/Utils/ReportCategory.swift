f4b7bb18a067727dfacbb79bb5134f26c025f39a
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-01-09 19:31:24 +0100

    Define enum for report category

---
+++ Ripple/Utils/ReportCategory.swift
@@ -0,0 +1,19 @@
+//
+//  ReportCategory.swift
+//  Ripple
+//
+//  Created by Dlovan Sharif on 2025-01-04.
+//
+
+import Foundation
+
+enum ReportCategory: String, CaseIterable {
+    case
+    harassment = "Harassment",
+    defamation = "Defamation",
+    offensivecontent = "Offensive Content",
+    phishing = "Phishing",
+    exploitation = "Exploitation",
+    abuse = "Abuse",
+    other = "Other"
+}
