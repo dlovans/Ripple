258e6ce460f665da9c266e48b64a6f0a8e6357e7
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-01-09 19:50:40 +0100

    Include report button in ChatMessageView

--- Ripple/Views/ChatView/ChatMessageView.swift
+++ Ripple/Views/ChatView/ChatMessageView.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 
 struct ChatMessageView: View {
+    @EnvironmentObject var chatViewModel: ChatViewModel
+    @EnvironmentObject var userViewModel: UserViewModel
     let username: String
+    let userId: String
     let message: String
     let isMe: Bool
     var isPremium = false
+    let messageId: String
+    
+    @State var displayMessageReport: Bool = false
     
     var body: some View {
         VStack (alignment: .leading) {
@@ -21,15 +27,41 @@ struct ChatMessageView: View {
             Text(message)
                 .foregroundStyle(isMe ? .black : .textcolor)
                 .font(.callout)
+            //            if userViewModel.user?.id != userId {
+            HStack {
+                Button {
+                    displayMessageReport = true
+                } label: {
+                    HStack (spacing: 0) {
+                        Group {
+                            Image(systemName: "exclamationmark.triangle.fill")
+                                .foregroundStyle(Color.orange)
+                            Text("Report")
+                                .foregroundStyle(Color.black)
+                        }
+                        .font(.footnote)
+                        .opacity(0.5)
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            //            }
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding(.horizontal, 10)
         .padding(.vertical, 5)
         .background(isMe ? Color.emerald : Color(red: 0.5, green: 0.5, blue: 0.5, opacity: 0.1))
         .clipShape(RoundedRectangle(cornerRadius: 10))
+        .sheet(isPresented: $displayMessageReport) {
+            ChatMessageReportView(
+                chatId: chatViewModel.chat?.id ?? "",
+                chatName: chatViewModel.chat?.chatName ?? "",
+                messageId: messageId,
+                againstUserId: userId,
+                byUserId: userViewModel.user?.id ?? "",
+                reportAgainstUsername: username,
+                displayMessageReport: $displayMessageReport
+            )
+        }
     }
-}
-
-#Preview {
-    ChatMessageView(username: "Dlovan", message: "chop chop", isMe: true)
 }
