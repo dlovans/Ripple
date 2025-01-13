3792e28cac1e373cd69fa5cd2cdc3f19078498c4
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-01-13 22:49:52 +0100

    Define ReportViewModel handling reporting between UI and database

---
+++ Ripple/ViewModels/ReportViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ReportViewModel.swift
+//  Ripple
+//
+//  Created by Dlovan Sharif on 2025-01-09.
+//
+
+import Foundation
+
+class ReportViewModel: ObservableObject {
+    private let reportRepository: ReportRepository = ReportRepository()
+    
+    func createMessageReport(chatId: String, messageId: String, againstUserId: String, byUserId: String, reportContent: String, reportType: ReportType) async -> ReportAndBlockStatus {
+        return await reportRepository.createMessageReport(chatId: chatId, messageId: messageId, againstUserId: againstUserId, byUserId: byUserId, reportContent: reportContent, reportType: reportType)
+    }
+}
