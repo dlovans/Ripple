a4296c70da52ce78cadbac50de2bac623868be19
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-01-13 22:47:15 +0100

    Define enumeration for report types (chat, message report)

---
+++ Ripple/Utils/ReportType.swift
@@ -0,0 +1,14 @@
+//
+//  ReportType.swift
+//  Ripple
+//
+//  Created by Dlovan Sharif on 2025-01-12.
+//
+
+import Foundation
+
+enum ReportType: String, Codable {
+    case
+    message,
+    chat
+}
