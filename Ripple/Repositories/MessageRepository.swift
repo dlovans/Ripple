0ece6e307fbd86df3fe1a6c8f4db3cdfddc10e2b
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-01-13 22:45:52 +0100

    Refactor to fix memory leaks

--- Ripple/Repositories/MessageRepository.swift
+++ Ripple/Repositories/MessageRepository.swift
@@ -10,39 +10,41 @@ import FirebaseFirestore
 
 class MessageRepository {
     let db = Firestore.firestore()
-        
-    func getMessages(chatId: String, onUpdate: @escaping ([Message]) -> Void) -> ListenerRegistration? {
+    
+    func getMessages(chatId: String, blockedUserIds: [String], onUpdate: @escaping ([Message]) -> Void) -> ListenerRegistration? {
         let messagesRef = db.collection("chats").document(chatId).collection("messages")
-        
+        print(blockedUserIds)
         let listener = messagesRef
-            .whereField( "timestamp", isGreaterThanOrEqualTo: Timestamp(date: Date.now))
-            .order(by: "timestamp").limit(to: 200)
+            .order(by: "timestamp")
+            .limit(to: 20)
             .addSnapshotListener { querySnapshot, error in
                 
-            if let error {
-                print("Failed to retrieve messages: \(error.localizedDescription)")
-                onUpdate([])
-                return
+                if let error {
+                    print("Failed to retrieve messages: \(error.localizedDescription)")
+                    onUpdate([])
+                    return
+                }
+                
+                guard let querySnapshot else {
+                    print("Error fetching messages snapshot: Snapshot is nil.")
+                    onUpdate([])
+                    return
+                }
+                
+                let messages = querySnapshot.documents
+                    .filter { !blockedUserIds.contains($0.data()["userId"] as? String ?? "Unknown")}
+                    .map { QueryDocumentSnapshot in
+                        Message(
+                            id: QueryDocumentSnapshot.documentID,
+                            userId: QueryDocumentSnapshot.data()["userId"] as? String ?? "Unknown",
+                            username: QueryDocumentSnapshot.data()["username"] as? String ?? "Unknown",
+                            message: QueryDocumentSnapshot.data()["message"] as? String ?? "Unknown",
+                            isPremium: QueryDocumentSnapshot.data()["isPremium"] as? Bool ?? false
+                        )
+                    }
+                
+                onUpdate(messages)
             }
-            
-            guard let querySnapshot else {
-                print("Error fetching messages snapshot: Snapshot is nil.")
-                onUpdate([])
-                return
-            }
-            
-            let messages = querySnapshot.documents.map { QueryDocumentSnapshot in
-                Message(
-                    id: QueryDocumentSnapshot.documentID,
-                    userId: QueryDocumentSnapshot.data()["userId"] as? String ?? "Unknown",
-                    username: QueryDocumentSnapshot.data()["username"] as? String ?? "Unknown",
-                    message: QueryDocumentSnapshot.data()["message"] as? String ?? "Unknown",
-                    isPremium: QueryDocumentSnapshot.data()["isPremium"] as? Bool ?? false
-                )
-            }
-            
-            onUpdate(messages)
-        }
         return listener
     }
     
@@ -57,7 +59,7 @@ class MessageRepository {
             "isPremium": isPremium,
             "timestamp": FieldValue.serverTimestamp()
         ]
-            
+        
         
         do {
             try await messagesRef.addDocument(data: messageData)
