a5169d54bdb45a4c8a5636b49365846825204397
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2025-01-16 13:16:27 +0100

    Update feature to update username

--- Ripple/Views/SettingsView/SettingsView.swift
+++ Ripple/Views/SettingsView/SettingsView.swift
@@ -13,6 +13,9 @@ struct SettingsView: View {
     @EnvironmentObject var chatViewModel: ChatViewModel
     @EnvironmentObject var locationService: LocationService
     @FocusState private var usernameIsFocused: Bool
+    @State private var logoutText: String = "Logout"
+    @State private var disableInputs: Bool = false
+    @State private var navigateToBlockList: Bool = false
     
     var locationEnabled: Bool {
         locationService.locationAuthorized == .authorizedAlways || locationService.locationAuthorized == .authorizedWhenInUse
@@ -21,68 +24,79 @@ struct SettingsView: View {
     @State var username: String = ""
     
     var body: some View {
-        ZStack {
-            Color.stone
-                .ignoresSafeArea()
-            VStack {
-                if locationEnabled {
-                    Text("Settings")
-                        .font(.title)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .foregroundStyle(.textcolor)
-                    ScrollView {
-                        VStack {
-                            UsernameView(usernameIsFocused: $usernameIsFocused)
+        NavigationStack {
+            ZStack {
+                Color.stone
+                    .ignoresSafeArea()
+                VStack {
+                    if locationEnabled {
+                        Text("Settings")
+                            .font(.title)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .foregroundStyle(.textcolor)
+                        ScrollView {
+                            VStack {
+//                                UsernameView(disableInputs: $disableInputs)
+                                BlockListButtonView(disableInputs: $disableInputs, navigateToBlockList: $navigateToBlockList)
+                                    .navigationDestination(isPresented: $navigateToBlockList) {
+                                        BlockListView()
+                                            .navigationBarBackButtonHidden(true)
+                                    }
+                            }
+                            
+                            Divider()
+                                .overlay(.white)
+                                .frame(width: 300)
+                                                    
+                            Button {
+                                Task { @MainActor in
+                                    self.disableInputs = true
+                                    let logoutStatus = authViewModel.logout()
+                                    if logoutStatus {
+                                        chatViewModel.stopListeningToChat()
+                                        chatViewModel.stopFetchingChats()
+                                        userViewModel.destroyUser()
+                                    } else {
+                                        logoutText = "Failed to logout. Try again later."
+                                    }
+                                }
+                            } label: {
+                                Text(logoutText)
+                                    .foregroundStyle(.textcolor)
+                                    .frame(maxWidth: .infinity)
+                            }
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(disableInputs ? Color.gray : Color.red)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .disabled(disableInputs)
                         }
-                        Divider()
-                            .overlay(.white)
-                            .frame(width: 300)
-                        Button {
-                            let logoutStatus = authViewModel.logout()
-                            if logoutStatus {
-                                chatViewModel.stopListeningToChat()
-                                chatViewModel.stopFetchingChats()
-                                userViewModel.destroyUser()
+                        .scrollIndicators(.hidden)
+                        .scrollDismissesKeyboard(.interactively)
+                    } else {
+                        Button{
+                            if let appSettings = URL(string: UIApplication.openSettingsURLString) {
+                                UIApplication.shared.open(appSettings, options: [:], completionHandler: nil)
                             }
                         } label: {
-                            Text("Logout")
-                                .foregroundStyle(.textcolor)
-                                .frame(maxWidth: .infinity)
+                            Text("Enable Location to use this app!")
+                                .foregroundStyle(.white)
                         }
                         .padding()
                         .frame(maxWidth: .infinity)
-                        .background(Color.red)
+                        .background(.emerald)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding()
                     }
-                    .scrollIndicators(.hidden)
-                    .scrollDismissesKeyboard(.interactively)
-                } else {
-                    Button{
-                        if let appSettings = URL(string: UIApplication.openSettingsURLString) {
-                            UIApplication.shared.open(appSettings, options: [:], completionHandler: nil)
-                        }
-                    } label: {
-                        Text("Enable Location to use this app!")
-                            .foregroundStyle(.white)
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(.emerald)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .padding()
+                }
+                .frame(alignment: locationEnabled ? .top : .center)
+                .padding()
+                
+                if !userViewModel.userLoaded {
+                    SpinnerView()
                 }
             }
-            .frame(alignment: locationEnabled ? .top : .center)
-            .padding()
-            .onTapGesture {
-                if locationEnabled && usernameIsFocused {
-                    usernameIsFocused = false
-                }
-            }
-            
-            if !userViewModel.userLoaded {
-                SpinnerView()
-            }
+
         }
     }
 }
